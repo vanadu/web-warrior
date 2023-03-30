c85a2f1feb8114ab6dcb3dc17c94246b1fbf97e7
Van Albert <motorhobo@gmail.com>
2023-03-30 11:59:39 -0400

    Before chapter 17

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,8 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  * 
  * // !VA This is where you configure the site metadata, that normally appears in the index.html file. There is no user-accessible index.html file in Gatsby. All the meta configuration happens here. Every time this config file is changed, the dev server needs to be restarted!!! Ugh. Here, the siteMetadata appears now in the GraphiQL interface under the 'site' property. In GraphiQL, Check the boxes for the properties you want to query and click to copy the query. You can now include the query in the page component.
+ * // !VA Chapter 16: register the two new images plugins below AND add a source-filesystem instance for the images, reboot the server.
+ * 
  * 
  */
 
@@ -11,11 +13,21 @@ module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
         path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
       },
     },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,17 @@ import { graphql, Link } from 'gatsby'
 import Layout from "../components/Layout"
 // !VA Chapter 6: This is the +V3 syntax of style modules import, ES6 standard. Module files have to have this naming structure. This is basic React.
 import * as styles from '../styles/home.module.css'
+import Img from 'gatsby-image'
 
 // !VA Chapter 3: Use standard React Link component for anchors
 // !VA Chapter 7: use img tags with source pointing to the public folder. You could just put any assets in the static folder, but it's not the best way in Gatsby because .
 // !VA Chapter 9: Export the GraphQL query to use it. I don't know why it's graphql here but graphiql in the browser. Import graphql from 'gatsby' and export the query inside backticks as below. Then destructure out the data property from the component props, destructure out the title and description properties of the data property, then you can render them in the component. 
 // !VA Chapter 10: get rid of the the calls to GraphQL to get the data prop to get the title and description to render in the p tag below, we're not using that anymore. It's in the Navbar component now.
 // export default function Home({data}) {
-export default function Home() {
+// !VA Chapter 16, now we need to destructure out the data object from the props returned from the image query at the bottom. To find out wherre to point the Img component to, drill down in the file object of the data prop. data -> file -> childImageSharp -> fluid.
+export default function Home({ data }) {
   // !VA Chapter10: refactoring out the data props below, not needed anymore.
-  // !VA Log the data props
-  // console.log(data);
+  console.log(data);
   // const { title, description } = data.site.siteMetadata
   return (
     <Layout>
@@ -23,19 +24,35 @@ export default function Home() {
           <p>UX designer & web developer based in Manchester.</p>
           <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
-        <img src="/banner.png" alt="site banner" style={{ maxWidth: '100%' }} />
+        {/* <img src="/banner.png" alt="site banner" style={{ maxWidth: '100%' }} /> */}
+        <Img fluid={data.file.childImageSharp.fluid}/>
+        
         {/* <p>{title } - { description } </p> */}
       </section>
     </Layout>
   )
 }
 
+// !VA Chapter 10 : Delete the query that returns the data prop, we dont' need it any more for this lesson
+// export const query = graphql`
+// query SiteInfo {
+//   site {
+//     siteMetadata {
+//       description
+//       title
+//     }
+//   }
+// }
+// `
+
+// !VA Chapter 16: The following query is a) renamed from the default Query to Banner and 2) has a 'fragment' ...GatsbyImageSharpFluid the includes all the default properties that were provided in the GraphiQL query, i.e. sizes, src, and srcSet. GatsbyImageSharpFluid includes all of these query properties in the GatsbyImageSharpFluid fragment. IMPORTANT: You have to generate this as a 'file' query! childImageSharp is available through the file property. ImageSharp is all over the place, but if you don't do it from the file property, it won't return a path to the file in the data object!
 export const query = graphql`
-query SiteInfo {
-  site {
-    siteMetadata {
-      description
-      title
+query Banner {
+  file(relativePath: {eq: "banner.png"}) {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid
+      }
     }
   }
 }
